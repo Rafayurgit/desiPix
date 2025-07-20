24a72ce4a38cdc65c6a97899cbe0036da122c842
farruop <rafayKhanpvt@gmail.com>
2025-07-20 16:30:17 +0530

    Add: loader and validation to input component

---
+++ frontend/src/components/ComponentLoader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ComponentLoader() {
+  return (
+    <div className='w-full sm:w-1/2 font-bold '>
+      Jugaad chalu hai bhai...
+    </div>
+  )
+}
