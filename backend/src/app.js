e0d84628a272cdd2bca6dc94dc53139de40f3db0
farruop <rafayKhanpvt@gmail.com>
2025-07-25 22:40:38 +0530

    Add: securee frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import imageRoutes from "./routes/image.route.js"
 
 const app= express();
 
-app.use(cors());
+app.use(cors({origin : process.env.FRONTEND_URL }));
 app.use(express.json());
 
 app.get("/", (req,res)=>{
