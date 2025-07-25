72827fe6dd82747fd00c147ca633007cf10e57af
farruop <rafayKhanpvt@gmail.com>
2025-07-26 02:37:03 +0530

    feat: add backend duplicate image detection using SHA256 hash

--- backend/src/controller/image.controller.js
+++ backend/src/controller/image.controller.js
@@ -1,25 +1,42 @@
 // import { convertImageFormat } from "../services/image.service.js";
+import fs, { readFileSync } from "fs";
+import crypto from "crypto";
 import {convertImageFormat} from "../services/image.service.js"
-import fs from "fs";
+
 
 export const convertImageController = async (req,res)=>{
 
+    const processedImages = new Set();
+
+    const getFileHash =(filepath)=>{
+        const fileBuffer = fs.readFileSync(filepath);
+        const hash = crypto.createHash("sha256").update(fileBuffer).digest("hex");
+        return hash;
+    }
+
     const file = req.file;
     const {Format} =req.body;
-
-    
 
     if(!file || !Format){
         return res.status(400).json({message:"File and format are required"})
     }
 
     try {
+        const fileHash = getFileHash(file.path);
+        const uniqueKey = `${fileHash}_${Format}`;
+
+        if(processedImages.has(uniqueKey)){
+            fs.unlinkSync(file.path)
+            return res.status(409).json({message:"Image has already been converted to this format."})
+        }
+
         const outputPath = await convertImageFormat(file.path, Format);
 
         if(!fs.existsSync(outputPath)){
             return res.status(500).json({message:"Conversion failed : Output not found"})
         }
 
+        processedImages.add(uniqueKey)
         
         res.download(outputPath, `converted.${Format}`, (error)=>{
             if(!error){
