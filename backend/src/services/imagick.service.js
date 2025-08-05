e87fb2cd9cf1ee1de5f2b7dc691aee69fa59709f
farruop <rafayKhanpvt@gmail.com>
2025-08-05 22:31:11 +0530

    Add: imagic magic for jpeg to heic conversition

--- backend/src/services/imagick.service.js
+++ backend/src/services/imagick.service.js
@@ -1,19 +1,31 @@
 import { exec } from "child_process";
 import util from "util";
 import path from "path";
+import { log } from "console";
+
 
 const execAsync = util.promisify(exec);
 
 export function canUseImageMagick(inputFormat, outputFormat) {
   const supported = new Set([
-    "tiff", "tif", "gif", "bmp", "ico", "svg", "pdf", "webp" // etc.
+    "tiff", "tif", "gif", "bmp", "ico", "svg", "pdf", "webp",
+    "jpeg", "jpg", "heic" // etc.
   ]);
   return supported.has(inputFormat) || supported.has(outputFormat);
 }
 
 export async function convertWithImageMagick(inputPath, outputFormat) {
   const outputPath = inputPath + "." + outputFormat;
-  const command = `magick convert "${inputPath}" "${outputPath}"`;
-  await execAsync(command);
-  return outputPath;
+  // const command = `magick convert "${inputPath}" "${outputPath}"`;
+  const command = `magick convert "${inputPath}" -quality 85 "${outputPath}"`;
+
+  try {
+    const { stdout, stderr } = await execAsync(command);
+    if(stderr) console.log("ImageMagick stderr:", stderr);
+    return outputPath
+    
+  } catch (error) {
+    console.log("ImageMagic command failed:", error);
+    throw error;
+  }
 }
