9b39542479551a08e82837a11ad3905ec6b78cd3
farruop <rafayKhanpvt@gmail.com>
2025-08-27 00:32:45 +0530

    add: seperate utility file for feteching response from backend

---
+++ frontend/src/services/apiService.js
@@ -0,0 +1,34 @@
+// services/apiService.js
+import axios from "axios";
+
+export async function uploadAndConvert(files, targetFormat, onProgress) {
+  const formData = new FormData();
+  
+  for (const file of files) {
+    formData.append("Image", file); // multiple "Image" entries
+  }
+  formData.append("Format", targetFormat);
+
+
+
+  const response = await axios.post("http://localhost:8080/upload", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+    responseType: "json",
+    timeout: 120000,
+    onUploadProgress: (event) => {
+      if (event.total && onProgress) {
+        const percent = Math.round((event.loaded * 100) / event.total);
+        onProgress(percent);
+      }
+    },
+  });
+
+//   return {
+//     blob: new Blob([response.data], { type: response.headers["content-type"] }),
+//     filename:
+//       response.headers["x-converted-filename"] ||
+//       file.name.replace(/\.[^.]+$/, "." + targetFormat),
+//   };
+
+  return response.data;
+}
