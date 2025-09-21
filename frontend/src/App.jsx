012fab82a2e41f32bb45aacc67b39bedc2b79ce9
farruop <rafayKhanpvt@gmail.com>
2025-09-21 09:23:22 +0530

    update: gradient style on app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,18 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import './App.css'  
-import Home from "./pages/Home"
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-
-
   return (
-
-
-    <div class="relative h-full w-full bg-gradient-to-br from-indigo-300 to-white "><div class=" absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] pointer-events-none"></div>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
-    </BrowserRouter>
-
-    
+    <div className="relative h-full w-full bg-gradient-to-br from-indigo-200 to-white ">
+      <div className=" absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] pointer-events-none"></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
