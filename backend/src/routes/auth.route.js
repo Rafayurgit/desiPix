5a9238511f2dec7a1f18c92f1d598dd42b1feb62
farruop <rafayKhanpvt@gmail.com>
2025-10-07 02:53:01 +0530

    Update: routes for Oauth

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,22 @@
 import { Router } from "express";
-import {signUp, signIn, logOut, refreshAcceeToken, changeCurrectPassword} from "../controller/auth.controller.js";
+import {signUp, signIn, logOut, refreshAccessToken, changeCurrentPassword} from "../controller/auth.controller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
+import { googleRedirect, googleCallback } from "../controller/auth.controller.js";
 
 const router = Router();
 
 router.route("/signUp").post(signUp);
 router.route("/signIn").post(signIn);
 
+//google routes
+router.get("/google", googleRedirect);
+router.get("/google/callback", googleCallback);
+
 //secured route
 router.route("/logOut").post(verifyJwt, logOut)
-router.route("/refresh").post(refreshAcceeToken)
-router.route("/changePassword").post(verifyJwt, changeCurrectPassword)
+router.route("/refresh").post(refreshAccessToken)
+router.route("/changePassword").post(verifyJwt, changeCurrentPassword)
+
 
 
 export default router
