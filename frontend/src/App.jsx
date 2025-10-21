8a35d3abca64a1b7813b0a98d2ba6c941a1f8243
farruop <rafayKhanpvt@gmail.com>
2025-10-21 21:59:48 +0530

    update routes and new pages, navbar initgration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,39 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import LoginPage from "./pages/Auth/LoginPage";
+import SignUpPage from "./pages/Auth/SignUpPage";
+import GoogleCallbackPage from "./pages/Auth/GoogleCallbackPage";
+import RequireAuth from "./pages/Auth/RequireAuth";
+import Dashboard from "./pages/Dashboard";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./common/NavBar";
 
 function App() {
   return (
     <div className="relative h-full w-full bg-gradient-to-br from-indigo-200 to-white ">
       <div className=" absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] pointer-events-none"></div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <AuthProvider>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signIn" element={<SignUpPage />} />
+            <Route
+              path="/auth/google/callback"
+              element={<GoogleCallbackPage />}
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <RequireAuth>
+                  <Dashboard />
+                </RequireAuth>
+              }
+            />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
